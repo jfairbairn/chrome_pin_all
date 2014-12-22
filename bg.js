72bd3bd4d7a973958769e8ac8651e755c15d62fe
James Fairbairn <james@netlagoon.com>
2014-12-22 22:47:58 +0000

    Hacked it together

---
+++ bg.js
@@ -0,0 +1,18 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({
+    currentWindow: true
+  }, function(tabs) {
+    var allPinned = true;
+    for (var i in tabs) {
+      if (! tabs[i].pinned) {
+        allPinned = false;
+        break;
+      }
+    }
+    console.log(allPinned);
+    for (var i in tabs) {
+      chrome.tabs.update(tabs[i].id, {pinned: !allPinned});
+    }
+  });
+
+});
